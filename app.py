3be82e04e8403be7d6e008eae9d8d066c552409e
hkhey <76151779+hkhey@users.noreply.github.com>
2023-12-29 13:16:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-import pickle
+import requests
+import joblib
 import numpy as np
 from flask import Flask, request, render_template
+from io import BytesIO
 
 app = Flask(__name__)
-model = pickle.load(open(r'C:\Users\acer\Documents\M2\Cloud native & AI\NewDataset\model.pkl', 'rb'))
+
+# GitHub raw content URL to the model file
+model_url = 'https://raw.githubusercontent.com/hkhey/Breast-cancer-prediction-APP/master/model.pkl'
+
+# Download the model file
+response = requests.get(model_url)
+
+# Check if the download was successful (status code 200)
+if response.status_code == 200:
+    # Load the model from the downloaded content
+    model = joblib.load(BytesIO(response.content))
+else:
+    # Handle the case when the download fails
+    print(f"Failed to download the model. Status code: {response.status_code}")
+    model = None  # You can set model to None or handle it differently based on your needs
+
 prediction_labels = {0: 'Benign', 1: 'Malignant'}
 
 @app.route('/')
@@ -12,6 +29,9 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if model is None:
+        return render_template('index.html', prediction_text='Error: Model not available')
+
     features = request.form.to_dict()
     features = list(features.values())
     features = list(map(float, features))
